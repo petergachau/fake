cd90cc1852d71be987a3bdbf2e3bc4f9f877d680
petergachau <petergachau57@gmail.com>
2022-02-20 21:49:46 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <div>
+  <h1>Heading.</h1>
+  <p>Paragraph</p>
+ <ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+  <li>wine</li>
+</ul>
+<h1 img src="dog.JPG" alt="pee"> </h1>
+</div>
+
+  ,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
